75ce4dd1215b3266a2f8dcb7be54abd149d0bb40
Daniel Ansari <daniel.ansari.2023@gmail.com>
2024-11-12 21:43:27 +0700

    add tailwind tools and daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+      },
+    },
+  },
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
+};
