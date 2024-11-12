3b5039a58998e622e11f8d2f350ee6db3ec94373
Daniel Ansari <daniel.ansari.2023@gmail.com>
2024-11-13 04:27:39 +0700

    fix lint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [:request_id]
+  config.log_tags = [ :request_id ]
 
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Only use :id for inspections in production.
-  config.active_record.attributes_for_inspect = [:id]
+  config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
@@ -103,7 +103,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   # Configure Solid Errors
-  config.solid_errors.connects_to = {database: {writing: :errors}}
+  config.solid_errors.connects_to = { database: { writing: :errors } }
   # config.solid_errors.send_emails = true
   # config.solid_errors.email_from = ""
   # config.solid_errors.email_to = ""
